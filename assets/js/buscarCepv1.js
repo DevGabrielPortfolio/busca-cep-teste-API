02035f8a6b0369a5c2c3b7ef97ae8a87da05bee3
Gabriel Belentani Felipe <gabriel.portifolio07@gmail.com>
2025-04-14 11:28:46 -0300

    ADD: Fazendo funcionar a lógica js e estilizando a aplicação

--- assets/js/buscarCepv1.js
+++ assets/js/buscarCepv1.js
@@ -1,56 +1,43 @@
-// Obstendo valor do campo
-const cepInput = document.querySelector('#cep');
+const cepInput= document.querySelector('#cep');
 
-// atribuindo funções nos botões
 const btnPesquisarCEP = document.querySelector('#btnPesquisar');
 const btnLimpar = document.querySelector('#btnLimpar');
 
-// adiciona um evento para verificar qual tecla foi pressionada
-cepInput.addEventListener('keypress', (event) =>{
 
-    // obtém o códgo ASCII da tecla pressionada
-    const keyCode = event.keyCode;
+cepInput.addEventListener('keypress', (event) => {
+    const keyCode=event.keyCode
+    console.log(keyCode)
     
-    // verificando se a tecla pressionada não é um número
-    if(keyCode < 48 || keyCode > 57){
+    if (keyCode < 48 || keyCode > 57) {
         event.preventDefault();
-        alert('Digite apenas números!');
+        alert("Digite Apenas Números.")
     }
+})
 
-});
-
-const obterDadosApi = async (cep) =>{
-    // armazenar o endereço de requisição da API
-    const urlAPI = `https://viacep.com.br/ws/'${cep}'/json/`;
-
-    // Armazenar a resposta da API e aguardar a construção dos dados
-    const response = await fetch(urlAPI);
-
-    // Converter os dados para JSON
+const obterDadosApi = async (cep) => {
+    const apiUrl=`https://viacep.com.br/ws/${cep}/json/`
+  
+    const response = await fetch(apiUrl)
+   
     const data = await response.json();
-
-    // Verificar se o CEP é válido
-    if(data.erro){
-        alert('O CEP digitado está inválido!');
-        return;
+  
+    if (data.erro){
+        alert("O CEP digitado está inválido.")
     }
     atribuirCampos(data);
-};
+}
 
-btnPesquisarCEP.addEventListener('click', (e)=>{
+btnPesquisarCEP.addEventListener('click', (e) => { 
     e.preventDefault();
-
-    // verifica se ó CEP tem 8 dígitos
-    if(cepInput.value.length != 8){
-        // Menos de 8 dígitos, exibe uma mensagem para o usuáro
-        alert('Por favor, digite um CEP válido com 8 dígitos.');
+    if (cepInput.value.length < 8 || cepInput.value.length > 9){
+        alert('Por favor, digite um CEP válido com 8 dígitos.')
         document.querySelector('#cep').value = '';
         return;
     }
     obterDadosApi(cepInput.value);
 });
 
-const atribuirCampos = (data) =>{
+const atribuirCampos = (data) => {
     const rua = document.querySelector('#rua');
     const complemento = document.querySelector('#complemento');
     const bairro = document.querySelector('#bairro');
@@ -59,16 +46,18 @@ const atribuirCampos = (data) =>{
 
     rua.value = data.logradouro;
     complemento.value = data.complemento;
-    bairro.value = data.bairro;
-    cidade.value = data.localidade;
-    estado.value = data.estado;
-};
+    bairro.value= data.bairro;
+    cidade.value= data.localidade;
+    estado.value= data.estado;
 
-btnLimpar.addEventListener('click', ()=>{
-    const cep = document.querySelector('#cep').value = '';
-    const rua = document.querySelector('#rua').value = '';
-    const complemento = document.querySelector('#complemento').value = '';
-    const bairro = document.querySelector('#bairro').value = '';
-    const cidade = document.querySelector('#cidade').value = '';
-    const estado = document.querySelector('#estado').value = '';
-});
+}
+
+btnLimpar.addEventListener('click', () => {
+    document.querySelector('#rua').value = ''
+    document.querySelector('#complemento').value=''
+    document.querySelector('#bairro').value=''
+    document.querySelector('#cidade').value=''
+    document.querySelector('#estado').value=''
+
+})
+
